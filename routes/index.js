492b934c55650520da90cdfff2fe996e09f569f2
n64 <n64stars@gmail.com>
2020-12-15 18:16:12 +0100

    added update points endpoints

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,14 @@ router.get("/", async (req, res) => {
   return res.status(200).json(players);
 });
 
+router.post("/", async (req, res) => {
+  let ppp;
+  for (ppp in req.body) {
+    const p = await Player.findById(ppp.playerId);
+    p.points.push(ppp.newPoints);
+    await p.save();
+  }
+  return res.status(200).json("Ok");
+});
+
 module.exports = router;
